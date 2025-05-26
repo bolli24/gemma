65b2f4ab3e70830fb215715e25f3bc06122803ce
bolli24 <4827765-bolli24@users.noreply.gitlab.com>
2025-05-26 14:45:43 +0200

    BROKEN: spawn balls with ecs.

--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,17 @@ const ecs = @import("ecs.zig");
 
 const ArrayList = std.ArrayList;
 
-const Ball = struct { pos: rl.Vector2, velocity: rl.Vector2, size: f32, color: rl.Color = rl.Color.red };
+const Circle = struct { size: f32, color: rl.Color = rl.Color.red };
+const Pos = struct { rl.Vector2 };
+const Velocity = struct { rl.Vector2 };
 
 pub fn main() anyerror!void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var prng = std.Random.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
 
     const screenWidth = 800;
     const screenHeight = 450;
@@ -25,13 +32,36 @@ pub fn main() anyerror!void {
 
     var player: rl.Vector2 = .{ .x = screenWidth * 0.5, .y = screenHeight * 0.5 };
     const player_speed = 200.0;
+    const base_speed = 200.0;
 
+    var world = ecs.World.init(gpa.allocator());
+    const rand = prng.random();
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
         //----------------------------------------------------------------------------------
-
         player = player.add(getMovement().scale(rl.getFrameTime() * player_speed));
+
+        if (rl.isKeyPressed(.space)) {
+            const entity = try world.create();
+            const dir = rl.Vector2.init(0, 1).rotate(rand.float(f32) * 2 * std.math.pi);
+            const speed = std.math.lerp(base_speed / 2, base_speed * 2, rand.float(f32));
+
+            const pos = rl.Vector2.init(
+                std.math.lerp(40, screenWidth - 40, rand.float(f32)),
+                std.math.lerp(40, screenHeight - 40, rand.float(f32)),
+            );
+
+            try world.add_component(entity, .{pos});
+            try world.add_component(entity, .{dir.scale(speed)});
+            try world.add_component(entity, Circle{
+                .size = rand.float(f32) * 20 + 3,
+                .color = .init(rand.int(u8), rand.int(u8), rand.int(u8), 255),
+            });
+
+            @compileLog(ecs.typeId(Pos));
+            @compileLog(ecs.typeId(Velocity));
+        }
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -39,9 +69,18 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
         rl.clearBackground(.white);
 
+        try world.runSystem(draw_circles);
+
         rl.drawRectangle(@intFromFloat(player.x), @intFromFloat(player.y), 20, 20, .red);
 
         //----------------------------------------------------------------------------------
+    }
+}
+
+fn draw_circles(query: *ecs.Query(struct { Pos, Circle })) void {
+    while (query.next()) |item| {
+        const pos, const circle = item;
+        rl.drawCircle(@intFromFloat(pos[0].x), @intFromFloat(pos[0].y), circle.size, circle.color);
     }
 }
 
