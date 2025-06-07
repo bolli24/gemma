5e816fef93b4248d67666cfac438558c90a41108
bolli24 <>
2025-06-08 00:23:31 +0200

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,23 +1,21 @@
-let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) {};
-in
-  {pkgs ? import <nixpkgs> {}}:
-    pkgs.mkShell {
-      nativeBuildInputs = with pkgs.buildPackages; [
-        zig
-        SDL2
-        pkg-config
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXi
-        xorg.libXext
-        xorg.libXfixes
-        wayland
-        pulseaudioFull
-        unzip
-        libxkbcommon
-        glfw
-      ];
-    }
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    zig
+    SDL2
+    pkg-config
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXi
+    xorg.libXext
+    xorg.libXfixes
+    wayland
+    wayland-scanner
+    pulseaudioFull
+    unzip
+    libxkbcommon
+    glfw
+  ];
+}
